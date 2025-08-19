7562cdfd40c29dd0f50a33353eb9a39a258c9b35
khushalsameer09-beep <khushalsameer09@gmail.com>
2025-08-19 18:50:45 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+#Task1
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
+#Task2
+firstname=input("Enter your first name: ")
+lastname=input("Enter your last name: ")
+c=firstname+" "+lastname
+print("Hello, "+c+"! Welocome to the Python Program.")
