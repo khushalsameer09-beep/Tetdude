acf89c390dcfd824539e1d3c3f24fbe83e9633e8
khushalsameer09-beep <khushalsameer09@gmail.com>
2025-08-26 23:03:04 +0530

    Add files via upload

---
+++ assignment 5.py
@@ -0,0 +1,14 @@
+#1
+'''students={"Khushal":85,"Vedant":60,"Rahul":76,"Rishu":88}
+name = input("Enter student's name: ")
+if name in students:
+    print(f"{name}'s marks= {students[name]}")
+else:
+    print("Student not found")'''
+#2
+num=list(range(1,11))
+first_5=num[:5]
+reversed_num=first_5[::-1]
+print("Origianl lists:",num)
+print("First 5 numbers are:",first_5)
+print("Reversed numbers are:",reversed_num)
